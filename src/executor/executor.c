e22a290a1433565b1b869a95765b25155facc0bb
Andrea Martins <andreamartins@Andreas-MacBook-Pro.local>
2024-04-08 21:54:26 +0200

    08042024

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -18,35 +18,73 @@
 
 void	ft_redir(t_toolkit *sh, t_pipe *p)
 {
+//verifica si el descriptor del archivo de entrada es mayor o igual a 0
 	if (p->in_fd >= 0)
 	{
+//duplica el descriptor de archivo y si hay error se va
 		if (dup2(p->in_fd, STDIN_FILENO) < 0)
 			err_exit(sh, "dup2", NULL, 1);
 		close(p->in_fd);
+		//cierra y se estable en -2 para indircar que no esta uso
 		p->in_fd = -2;
 	}
+//verifica si descriptor de salida es mayor igual a 0
 	if (p->out_fd >= 0)
 	{
+	//duplica y si hay error se va
 		if(dup2(p->out_fd,STDOUT_FILENO) < 0)
-			err_exit
+			err_exit(sh, "dup2", NULL, 1);
+		close(p->out_fd)
+		p->out_fd = -2;
 	}
 }
 
 void	child_process(t_toolkit *sh, t_pipe *p, int option)
 {
+//iniciar senhales
 	init_signals(N_INTERACT);
+	//si option es igual 0 se cierra el descriptor y se establece -2
 	if (!option)
 	{
 		close(sh->exe->fdp[0]);
 		sh->exe->fdp[0] = -2;
 	}
+	//manejar redireccion de archivos
 	ft_open(sh, p, p->fd_lst, -1 );
+	//si es verdadero se ejecuta exec builtin y se sale del proceso
 	if(p->builtin)
 		exit(exec_builting (sh,p));
+	//verificar si el usuario tiene el permisod e eejecutar el comando
 	check_access(sh, p->cmd, p);
+	//manejar la redireccion del preoceso hijo
 	ft_redir(sh, p);
+	//ejecutar el comando especificado y hay error se va
 	if(execve(p->path, p->cmd, sh->env) == -1)
 		err_exit(sh, "execve", NULL, 14);
+}
+
+int	last_child(t_toolkit *sh, t_pipe *p)
+{
+	//verificar si p->cmd es un comando integrado
+	p->builtin = check_builtin(p->cmd);
+	//verifica si no hay tuberias y si el ultimo elemento de la linea de tuberias es un cmd integrado
+	if(!sh->pipes && sh->pipe_lst->builtin)
+	{
+		sh->exit = exec_builtin(sh, p);
+		return(0);
+	}
+	//nuevo proceso hijo usando fork y le pid hijo se guarda
+	sh->exe->pid = fork();
+	//si el fork es negativo
+	if (sh->exe->pid < 0)
+		return(err_break(sh, "fork", NULL, 12));
+	//si el fork es igual a 0
+	else if(sh->exe->pid == 0)
+		child_proces(sh, p,1);
+	//si hay tuberias y el descriptor es mayor o igual a 0 
+	if (sh->pipes && p->in_fd >= 0)
+		close(p->in_fd);
+	return(0);
 }
 
 int	executor(t_toolkit *sh, t_pipe *p, int i, int j)
@@ -85,8 +123,10 @@ int	executor(t_toolkit *sh, t_pipe *p, int i, int j)
 
 int	exec_builtin(t_toolkit *sh, t_pipe *p)
 {
+	//verifica si no hay tuberias
 	if(!sh->pipes && ft_open_built(sh, p, p->fd_lst, -1))
 		return(sh->exit);
+	
 	if(p->builtin == 1)
 		return(ft_echo(sh, p))
 	if(p->builtin == 2)
@@ -97,6 +137,7 @@ int	exec_builtin(t_toolkit *sh, t_pipe *p)
 		return(ft_export(sh, p))	
 	if(p->builtin == 5)
 		return(ft_unset(sh, p))
+	//si builtin es igual a 6 y no hay paths
 	if(p->builtin == 6 && sh->paths)
 		return(ft_env(sh, p));
 	else if ( p->builtin == 6 && !sh->paths)
--- src/executor/executor_utils.c
+++ src/executor/executor_utils.c
@@ -35,13 +35,57 @@ int	check_builtin(char **cmd)
 
 // void	ft_open(t_toolkit *sh, t_pipe,)
 // void ft_check_open()
-// void check_access()
+void check_access()
+{
+	if(!cmd || !(*cmd))
+		err_exit(sh, NULL, NULL, 0);
+	else if (!(**cmd))
+		err_exit(sh, *cmd,"command not found", 127);
+	if (ft_strchr(cmd[0], '/'))
+	{
+		if(access(cmd[0], F_OK) == 0)
+		{
+			if(access(cmd[0], X_OK) != 0 )
+				err_exit(sh, cmd[0], "permission denied", 126);
+			p->path =cmd[0]
+		}
+		else
+			err_exit(sh,cmd[0], "No sucha afile or directory",127); 		
+	}
+	else
+		check_paths(sh->paths, cmd[0], sh, p);
+}
 
 
 void	check_paths()
 {
+	int i;
 
+	i = 0;
+	if(!paths)
+		err_exit(sh, cmd, "No such file or directory", 127);
+	while(paths[i])
+	{
+		//se crea la ruta competa
+		pipe->path = ft_smart_join(paths[i],"/",cmd);
+		//si no se pudo reservsr la memoria se da error
+		if(!pipe->path)
+			err_exit(sh, "malloc",NULL, 12);
+		//se verifica si el comando existe
+		if(access(pipe->path, F_OK) == 0)
+		{
+		//se verific si tiens permiso de ejecucion
+			if (access(pipe->path, X_OK) != 0)
+				err_exit(sh, cmd, "permission denied", 126);
+			else
+				return;	
+		}
+		//si no existe se libera la memoria
+		pipe->path = ft_memdel(pipe->path);
+		i++;
+	}
+	err_exit(sh,cmd, "command not found", 127);	
 }
 
 
- 
+
