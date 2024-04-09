a6d455486ad494b129e1f15dddb3688588db9ba3
Marti Marsa Samper <mmarsa-s@car13s2.42barcelona.com>
2024-04-09 17:07:56 +0200

    something

--- src/lexer/tools.c
+++ src/lexer/tools.c
@@ -45,3 +45,15 @@ int	word_in_quotes(char *input, char *q, int j)
 		j++;
 	return (j);
 }
+
+int	len_no_q(char *str, char q, int len, int j)
+{
+	
+}
+
+char	*trim_quotes(char *str, char q, int len, int j)
+{
+	if (!str || !len || (!ft_strchr(str, 34) && !ft_strchr(str, 39)))
+		return (str);
+			
+}
