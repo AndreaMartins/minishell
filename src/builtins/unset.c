8aea2c3f26d8948049ab6c3817039d3f68425471
Andrea Martins <andmart2@car9s6.42barcelona.com>
2024-04-29 13:01:01 +0200

    unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -12,3 +12,54 @@
 
 #include "../../includes/minishell.h"
 
+void    unset_free(t_env *env)
+{
+    ft_memdel(env->env_key);
+    ft_memdel(env->env_val);
+    ft_memdel(env);
+}
+
+void unset_var(t_mini *sh,char *var)
+{
+    t_env *node;
+    t_env *tmp;
+
+    node = sh->env_lst;
+    tmp = NULL;
+    if(ft_strcmp(node->env_key, var) == 0)
+    {
+        sh->env_lst = sh->env_lst->next;
+        unset_free(node);
+        return;
+    }
+    while(node != NULL && ft_strcmp(node->env_key, var) != 0)
+    {
+        tmp = node;
+        node = node-next;
+    }
+    if(node == NULL)
+        return;
+    tmp->next = node->next;
+    unset_free(node);
+}
+
+int ft_unset(t_mini *sh, t_pipe *p)
+{
+    char    **args;
+    int     i;
+
+    (void)p;
+    args = sh->pipe_lst->cmd;
+    i = 1;
+    while (args[i])
+    {
+        unset_var(sh, args[i]);
+        i++;
+    }
+    if (sh->env)
+        sh->env = arr_clean(sh->env, o);
+    sh->env =env_converter(sh->env_lst);
+    if (!sh->env)
+        return(err_break(sh, "malloc", NULL, 12));
+    return(0);
+}
