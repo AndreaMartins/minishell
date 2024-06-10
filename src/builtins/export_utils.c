2ead0c31a324b5ff386679aa8eed1c83facbe66f
Andrea Martins <andmart2@car14s6.42barcelona.com>
2024-06-10 12:49:09 +0200

    builtins and executor normi

--- src/builtins/export_utils.c
+++ src/builtins/export_utils.c
@@ -21,30 +21,30 @@ void	print_env_fd(t_env *tmp, int output)
 	ft_putstr_fd("\n", output);
 }
 
-int print_export(t_env *eprint, t_pipe *p)
+int	print_export(t_env *eprint, t_pipe *p)
 {
-    t_env   *tmp;
-    int output;
+	t_env	*tmp;
+	int		output;
 
-    output = p->out_fd;
-    if (output < 1)
-        output = 1;
-    if (!eprint)
-        return(1);
-    print_sort_print(eprint);
-    tmp = eprint;
-    while (tmp)
-    {
-        ft_putstr_fd("declare -x", output);
-        ft_putstr_fd(tmp->key, output);
-        if(tmp->val)
-            print_env_fd(tmp, output);
-        else
-            ft_putstr_fd("\n", output);
-        tmp = tmp->next;
-    }
-    tmp = eprint;
-    return(0);
+	output = p->out_fd;
+	if (output < 1)
+		output = 1;
+	if (!eprint)
+		return (1);
+	print_sort_print(eprint);
+	tmp = eprint;
+	while (tmp)
+	{
+		ft_putstr_fd("declare -x", output);
+		ft_putstr_fd(tmp->key, output);
+		if (tmp->val)
+			print_env_fd(tmp, output);
+		else
+			ft_putstr_fd("\n", output);
+		tmp = tmp->next;
+	}
+	tmp = eprint;
+	return (0);
 }
 
 char	*find_in_env_variables(t_toolkit *sh, char *variable_name)
--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -10,25 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../includes/minishell.h"
 
-int ft_pwd(t_toolkit *sh, t_pipe *p)
+int	ft_pwd(t_toolkit *sh, t_pipe *p)
 {
-    char *pwd;
-    int output;
+	char	*pwd;
+	int		output;
 
-    output = p->out_fd;
-    if(output < 0)
-        output = 1;
-    pwd = NULL;
-    pwd = getcwd (pwd, 0);
-    if (!pwd)
-    {
-        ft_putstr_fd(ft_get_value(sh, "PWD"), output);
-    }
-    ft_putstr_fd(pwd, output);
-    ft_putstr_fd("\n", output);
-    free(pwd);
-    return(0);
+	output = p->out_fd;
+	if (output < 0)
+		output = 1;
+	pwd = NULL;
+	pwd = getcwd(pwd, 0);
+	if (!pwd)
+	{
+		ft_putstr_fd(ft_get_value(sh, "PWD"), output);
+	}
+	ft_putstr_fd(pwd, output);
+	ft_putstr_fd("\n", output);
+	free(pwd);
+	return (0);
 }
