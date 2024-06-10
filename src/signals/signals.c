770204a4751e8849ac23e51d0ac2e76fa3e139c3
Andrea Martins <andmart2@car14s6.42barcelona.com>
2024-06-10 13:03:16 +0200

    signals normi

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -11,31 +11,29 @@
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-
 void	norm_handler(int sig, siginfo_t *data, void *non_used_data)
 {
-	(void) data;
-	(void) non_used_data;
-
-	if(sig == SIGINT)
+	(void)data;
+	(void)non_used_data;
+	if (sig == SIGINT)
 	{
 		printf("\n");
 		rl_on_new_line();
-		rl_replace_line("",1);
+		rl_replace_line("", 1);
 		rl_redisplay();
-		g_sig_rec = 1; 
+		g_sig_rec = 1;
 	}
-	return;
+	return ;
 }
 
 void	do_sigign(int signum)
 {
-	struct sigaction signal;
+	struct sigaction	signal;
 
 	signal.sa_handler = SIG_IGN;
 	signal.sa_flags = SA_RESTART;
 	sigemptyset(&signal.sa_mask);
-	if (sigaction(signum, &signal, NULL)< 0)
+	if (sigaction(signum, &signal, NULL) < 0)
 	{
 		exit(1);
 	}
@@ -43,15 +41,15 @@ void	do_sigign(int signum)
 
 int	init_signals(int mode)
 {
-	struct sigaction signal;
+	struct sigaction	signal;
 
 	signal.sa_flags = SA_RESTART | SA_SIGINFO;
 	sigemptyset(&signal.sa_mask);
-	if(mode == NORM)
+	if (mode == NORM)
 		signal.sa_sigaction = norm_handler;
 	sigaction(SIGINT, &signal, NULL);
 	sigaction(SIGQUIT, &signal, NULL);
-	return(0);
+	return (0);
 }
 
 void	exit_status(t_toolkit *sh, int j)
@@ -60,16 +58,16 @@ void	exit_status(t_toolkit *sh, int j)
 	{
 		if (sh->exe->pid == wait(&sh->exe->stat))
 		{
-			if(WIFEXITED(sh->exe->stat))
+			if (WIFEXITED(sh->exe->stat))
 				sh->exit = WIFEXITED(sh->exe->stat);
-			else if(WIFSIGNALED(sh->exe->stat))
+			else if (WIFSIGNALED(sh->exe->stat))
 			{
-				if(WTERMSIG(sh->exe->stat)== SIGINT)
+				if (WTERMSIG(sh->exe->stat) == SIGINT)
 				{
 					printf("\n");
 					sh->exit = 130;
 				}
-				else if(WTERMSIG(sh->exe->stat) == SIGQUIT)
+				else if (WTERMSIG(sh->exe->stat) == SIGQUIT)
 				{
 					sh->exit = 131;
 					printf("Quit: 3\n");
